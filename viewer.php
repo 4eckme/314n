898e8fe6278ec37e31fc295c096796fbf3aa3b30
4eckme <60138564+4eckme@users.noreply.github.com>
2020-04-21 12:18:58 +0300

    Create viewer.php

---
+++ viewer.php
@@ -0,0 +1,48 @@
+<?php
+mb_internal_encoding("UTF-8");
+mb_regex_encoding("UTF-8");
+class Viewer
+{
+    private function Viewer() { }
+    
+    static function message($message) {
+        
+        echo json_encode(array(
+                'clear'=>0,
+                'message'=>'<div class="message">'.$message.'</div>',
+                'path'=>''
+        ));
+    }
+    
+    static function error($error) {
+        
+        echo json_encode(array(
+                'clear'=>0,
+                'message'=>'<div class="error">'.$error.'</div>',
+                'path'=>''
+        ));
+    }
+    
+    static function content($content, $path) {
+        //$content = mb_convert_encoding($content, 'utf-8', 'ASCII');
+        $content = mb_ereg_replace('  ', ' ', $content);
+        $path = mb_ereg_replace(' ', '&nbsp;', $path);
+        
+        echo json_encode(array(
+                'clear'=>1,
+                'message'=>'<div id="loading"></div><div class="content" style="display:none">'.$content.'</div>',
+                'path'=> $path
+        ));
+    }
+    
+    static function edit($post_id, $content) {
+        echo json_encode(array(
+                'clear'=>0,
+                'edit'=>1,
+                'edittext'=>  $content,
+                'path'=>'POST&nbsp;'.$post_id
+        ));
+    }
+}
+
+?>
