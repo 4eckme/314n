e5973188f85fb5bcf2f7772fce6c9bd826a6cd53
4eckme <60138564+4eckme@users.noreply.github.com>
2020-04-21 12:38:16 +0300

    Create structure.sql

---
+++ structure.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Апр 21 2020 г., 12:05
+-- Версия сервера: 10.1.44-MariaDB-0+deb9u1
+-- Версия PHP: 5.6.40-25+0~20200224.31+debian9~1.gbp0b752b
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `clear314n`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `boards`
+--
+
+CREATE TABLE `boards` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `public` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `codes`
+--
+
+CREATE TABLE `codes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `code` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `invites`
+--
+
+CREATE TABLE `invites` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `invite` varchar(16) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `board_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `changing_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `creation_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `bump_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `timezone` varchar(6) NOT NULL DEFAULT '+04:00',
+  `ip` varchar(15) NOT NULL DEFAULT '0.0.0.0',
+  `banned` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_posts_rv`
+--
+
+CREATE TABLE `users_posts_rv` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `visited_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_topics_rv`
+--
+
+CREATE TABLE `users_topics_rv` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `board_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `boards`
+--
+ALTER TABLE `boards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `codes`
+--
+ALTER TABLE `codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `invites`
+--
+ALTER TABLE `invites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `topic_id` (`topic_id`),
+  ADD KEY `topic_id_2` (`topic_id`,`creation_date`),
+  ADD KEY `topic_id_3` (`topic_id`),
+  ADD KEY `topic_id_4` (`topic_id`),
+  ADD KEY `topic_id_5` (`topic_id`),
+  ADD KEY `topic_id_6` (`topic_id`),
+  ADD KEY `topic_id_7` (`topic_id`),
+  ADD KEY `topic_id_8` (`topic_id`),
+  ADD KEY `topic_id_9` (`topic_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users_posts_rv`
+--
+ALTER TABLE `users_posts_rv`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users_topics_rv`
+--
+ALTER TABLE `users_topics_rv`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `boards`
+--
+ALTER TABLE `boards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT для таблицы `codes`
+--
+ALTER TABLE `codes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2876;
+
+--
+-- AUTO_INCREMENT для таблицы `invites`
+--
+ALTER TABLE `invites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10025;
+
+--
+-- AUTO_INCREMENT для таблицы `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16498;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5002;
+
+--
+-- AUTO_INCREMENT для таблицы `users_posts_rv`
+--
+ALTER TABLE `users_posts_rv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14863;
+
+--
+-- AUTO_INCREMENT для таблицы `users_topics_rv`
+--
+ALTER TABLE `users_topics_rv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
